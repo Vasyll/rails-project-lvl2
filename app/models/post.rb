114228764b2239c58aca64d16348fd8015004de9
Vyacheslav Ryzhov <chjusp@gmail.com>
2022-05-19 19:50:20 +0300

    Consider merging nested conditions into outer  conditions in show.html.slim

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 50 }
-  validates :category_id, presence: true
 
   belongs_to :category
   belongs_to :creator, class_name: 'User'
