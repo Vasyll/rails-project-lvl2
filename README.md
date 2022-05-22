5d9693334a79648158ea2847be221866a78872fc
Vyacheslav Ryzhov <chjusp@gmail.com>
2022-05-22 18:16:48 +0300

    Returned post tests

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Vasyll/rails-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Vasyll/rails-project-lvl2/actions)
+![CI status](https://github.com/Vasyll/rails-project-lvl2/actions/workflows/master.yml/badge.svg)
 
 https://rails-project-lvl2-403793.herokuapp.com/
---
+++ test/controllers/post_controller_test.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class PostControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @post = posts(:one)
+    @user = users(:one)
+    @attrs = {
+      title: Faker::Book.title,
+      body: Faker::Books::Dune.quote,
+      category_id: categories(:one).id
+    }
+  end
+
+  test 'should get index' do
+    get posts_url
+    assert_response :success
+  end
+
+  test 'should show post' do
+    get post_url @post
+    assert_response :success
+  end
+
+  test 'guest should raise error from new' do
+    get new_post_url
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'signin user should get new' do
+    sign_in @user
+    get new_post_url
+    assert_response :success
+  end
+
+  test 'guest cant create post' do
+    post posts_url, params: { post: @attrs }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'signed user can create post' do
+    sign_in @user
+
+    post posts_url, params: { post: @attrs }
+    assert_response :redirect
+
+    blog_post = Post.find_by! title: @attrs[:title]
+    assert { blog_post.creator == @user }
+
+    assert_redirected_to post_url(blog_post)
+  end
+end
